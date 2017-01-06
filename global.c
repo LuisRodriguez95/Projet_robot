dc0d52621dc7963391df9b5a1e0ff881806e88eb
LuisRodriguez95 <luisrodval1995@gmail.com>
2017-01-06 18:19:39 +0100

    Add files via upload
    Files have batterie et watchdog commente

---
+++ global.c
@@ -0,0 +1,41 @@
+/*
+ * File:   global.h
+ * Author: pehladik
+ *
+ * Created on 21 avril 2011, 12:14
+ */
+
+#include "global.h"
+
+RT_TASK tServeur;
+RT_TASK tconnect;
+RT_TASK tmove;
+RT_TASK tenvoyer;
+
+RT_MUTEX mutexEtat;
+RT_MUTEX mutexMove;
+RT_MUTEX mutexCompteur;
+
+RT_SEM semConnecterRobot;
+
+RT_QUEUE queueMsgGUI;
+
+int etatCommMoniteur = 1;
+int etatCommRobot = 1;
+int Compteur = 0;
+DRobot *robot;
+DMovement *move;
+DServer *serveur;
+
+
+int MSG_QUEUE_SIZE = 10;
+
+int INCREMENTER = 1;
+
+int REINITIALISER = 0;
+int CPT_MAX = 3;
+
+int PRIORITY_TSERVEUR = 30;
+int PRIORITY_TCONNECT = 20;
+int PRIORITY_TMOVE = 10;
+int PRIORITY_TENVOYER = 25;
